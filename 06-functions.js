bbc8730772277827cca6080baff048e04ed5d519
Sung Jinwoo <abdurrahmanabdulkabir06@gmail.com>
2025-05-18 08:25:59 +0100

    functions

---
+++ 06-functions.js
@@ -0,0 +1,11 @@
+// FUNCTIONS
+
+// Function declaration
+function greet(name) {
+  return "Hello, " + name + "!";
+}
+console.log(greet("Alice"));
+
+// Arrow function
+const add = (a, b) => a + b;
+console.log(add(3, 5)); // 8
